0927e63a6711268254be18cb241777af7523b286
Maxxus220 <Maxxus220@gmail.com>
2022-08-24 19:14:34 -0500

    Working on turning svg to png

--- Scraper.py
+++ Scraper.py
@@ -6,25 +6,48 @@ from selenium.webdriver.common.keys import Keys
 
 from webdriver_manager.chrome import ChromeDriverManager
 
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
+
+import os
 import time
 import requests
 import shutil
 
-# def requestURL():
-#     return input("Enter a musescore url: ")
+def requestURL():
+    return input("Enter a musescore url: ")
 
-# def requestSaveName():
-#     return input("Enter a name to save the song under: ")
+def requestSaveName():
+    return input("Enter a name to save the song under: ")
 
-# def scrape():
-#     driver = webdriver.Chrome()
-#     driver.get("http://selenium.dev")
-#     time.sleep(3)
-#     driver.quit()
+def detectScoreType(scoreUrl):
+    if 'svg' in scoreUrl:
+        return 'svg'
+    elif 'png' in scoreUrl:
+        return 'png'
+    else:
+        return None
+    
+def downloadScore(src, saveName):
+    scoreType = detectScoreType(src)
+    r = requests.get(src, stream=True)
+    if r.status_code == 200 or scoreType == None:
+        r.raw.decode_content = True
+        with open('./Music/' + saveName + '_' + str(score_num) + '.' + scoreType, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+            f.close()
+            if scoreType == 'svg':
+                svg = svg2rlg('./Music/' + saveName + '_' + str(score_num) + '.' + scoreType)
+                renderPM.drawToFile(svg, './Music/' + saveName + '_' + str(score_num) + '.png')
+                os.remove('./Music/' + saveName + '_' + str(score_num) + '.' + scoreType)
+            
+        print(saveName + '_' + str(score_num) + '.png successfully downloaded')
+    else:
+        print('Problem encountered while trying to download ' + saveName + str(score_num))
+    
 
-# url = requestURL()
-# saveName = requestSaveName()
-# scrape()
+url = requestURL()
+saveName = requestSaveName()
 
 # driver options
 WINDOW_SIZE = "1920,1080"
@@ -34,7 +57,7 @@ chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
 # initialize chrome driver
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-driver.get("https://musescore.com/user/16006641/scores/4197961")
+driver.get(url)
 
 # select div containing sheet music
 body = driver.find_element(By.TAG_NAME, "body")
@@ -49,29 +72,22 @@ last_height = driver.execute_script("return document.getElementById(\"jmuse-scro
 while True:
     body.send_keys(Keys.PAGE_DOWN)
     time.sleep(0.5)
+    
+    # check for score elements
     scores = driver.find_elements(By.CLASS_NAME, 'Hn_kk')
-    print(len(scores))
+    
+    # check for next score
     for score in scores:
         src = score.get_attribute('src')
         if 'score_' + str(score_num) in src:
-            print(src)
+            downloadScore(src, saveName)
             score_num+=1
+            
+    # check for end of scrolling
     new_height = driver.execute_script("return document.getElementById(\"jmuse-scroller-component\").scrollTop")
     if new_height == last_height:
         break
     last_height = new_height
-    
-# scores = driver.find_elements(By.TAG_NAME, 'img')
-# print(len(scores))
-# for score in scores:
-#     src = score.get_attribute('src')
-#     print(src)
-    # print(src)
-    # r = requests.get(src, stream=True)
-    # if r.status_code == 200:
-    #     r.raw.decode_content = True
-    #     with open("score", 'wb') as f:
-    #         shutil.copyfileobj(r.raw, f)
 
-time.sleep(3)
+input('Press ENTER to quit')
 driver.quit()
